559428f6f2506212be08224025ed28b3a5cbf700
Karandi Tamas <tkarandi@gmail.com>
2014-10-13 13:00:26 +0200

    Add filter (top100) and sort (by count) the result for byDomain mapping

--- src/main/java/hu/evosoft/statservice/endpoint/StatController.java
+++ src/main/java/hu/evosoft/statservice/endpoint/StatController.java
@@ -5,6 +5,9 @@ import hu.evosoft.statservice.pojo.LogEntryDate;
 import hu.evosoft.statservice.repository.DestinationHostRepository;
 import hu.evosoft.statservice.repository.LogEntryDateRepository;
 
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -28,16 +31,34 @@ public class StatController {
 	@RequestMapping(value = "/byTime", method = RequestMethod.GET)
 	List<LogEntryDate> byTime() {
 		List<LogEntryDate> found = logEntryDateRepository.findAll();
-		System.out.println("StatByTimeController.list() " + found.size());
 		return found;
 	}
 	
 	@RequestMapping(value = "/byDomain", method = RequestMethod.GET)
 	List<DestinationHost> byDomain() {
 		List<DestinationHost> found = destinationHostrepository.findAll();
-		System.out.println("StatByDomainController.list(): " + found.size());
-		return found;
+		found.sort(DestinationHostComparator.INSTANCE);
+		
+		List<DestinationHost> top100 = new LinkedList<DestinationHost>();
+		Iterator<DestinationHost> iterator = found.iterator();
+		int threshold = 100;
+		int current = 0;
+		while (iterator.hasNext() && current < threshold) {
+			top100.add(iterator.next());
+			current++;
+		}
+		return top100;
 	}
 
+	/**
+	 * Sorts the 'download by domain' statistics in a descending order. 
+	 */
+	private enum DestinationHostComparator implements Comparator<DestinationHost> {
+		INSTANCE;
+		
+		public int compare(DestinationHost o1, DestinationHost o2) {
+			return o2.getValue()  - o1.getValue();
+		}
+	}
 	
 }
